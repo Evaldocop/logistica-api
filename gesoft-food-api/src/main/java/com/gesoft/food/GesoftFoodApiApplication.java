dc7cbb1cfe394151774291f855ddd3bddb945ad9
Evaldo <evaldocop@yahoo.com.br>
2022-09-13 15:28:16 -0300

    first commit

---
+++ gesoft-food-api/src/main/java/com/gesoft/food/GesoftFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.gesoft.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesoftFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesoftFoodApiApplication.class, args);
+	}
+
+}
---
+++ gesoft-food-api/src/test/java/com/gesoft/food/GesoftFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gesoft.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesoftFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
