697280cb1da3e9d11626e968326d1c901a2601af
Evaldo <evaldocop@yahoo.com.br>
2022-10-21 14:34:12 -0300

    mapeando Grupo, Usuario e Permissão

---
+++ gesoft-food-api/src/main/java/com/gesoft/food/domain/model/Grupo.java
@@ -0,0 +1,31 @@
+package com.gesoft.food.domain.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Grupo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	private String nome;
+	
+	@ManyToMany
+	@JoinTable(name = "grupos_permissoes", joinColumns = @JoinColumn(name="grupo_id"),
+	                                       inverseJoinColumns = @JoinColumn(name="restaurante_id"))
+	private List<Permissao> permissoes;
+
+}
