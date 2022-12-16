2ae9ec8c8a7a24e16836a16475ab689ec73af584
Evaldo <evaldocop@yahoo.com.br>
2022-12-16 09:32:36 -0300

    23.5 - funcionando

--- gesoft-auth/src/main/java/com/gesoft/gesoftauth/AuthorizationServerConfig.java
+++ gesoft-auth/src/main/java/com/gesoft/gesoftauth/AuthorizationServerConfig.java
@@ -3,8 +3,8 @@ package com.gesoft.gesoftauth;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,8 +15,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableAuthorizationServer
@@ -33,8 +32,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired(required = false)
 	private UserDetailsService userDetailsService;
 	
-	@Autowired
-	private RedisConnectionFactory redisConnectionFactory;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -92,14 +89,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.authenticationManager(authenticationManager)
 			.userDetailsService(userDetailsService)
 			.reuseRefreshTokens(false)
-			.tokenStore(redisTokenStore())
+			.accessTokenConverter(jwtAccessTokenConverter())
 			.tokenGranter(tokenGranter(endpoints));
 	}
 	
-	private TokenStore redisTokenStore() {
-		return new RedisTokenStore(redisConnectionFactory);
-	}
-	
+	@Bean
+	public JwtAccessTokenConverter  jwtAccessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("20131show");
+		return jwtAccessTokenConverter;
+		
+	}	
 	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
 		var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
 				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
@@ -111,11 +111,5 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		return new CompositeTokenGranter(granters);
 	}
 	
-	 
-	/*public JwtAccessTokenConverter  jwtAccessTokenConverter() {
-		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		jwtAccessTokenConverter.setSigningKey("20131show");
-		return jwtAccessTokenConverter;
-		
-	}*/
+
 }
